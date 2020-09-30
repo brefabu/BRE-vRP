2191ee231ed5b61a0ff11adb1f0dff4281230170
brefaby <40739200+brefaby@users.noreply.github.com>
2020-10-01 02:32:15 +0300

    Add files via upload

---
+++ mysql-async/lib/MySQL.lua
@@ -0,0 +1,300 @@
+MySQL = {
+    Async = {},
+    Sync = {},
+	Threaded = {}
+}
+
+local function safeParameters(params)
+    if nil == params then
+        return {[''] = ''}
+    end
+
+    assert(type(params) == "table", "A table is expected")
+    assert(params[1] == nil, "Parameters should not be an array, but a map (key / value pair) instead")
+
+    if next(params) == nil then
+        return {[''] = ''}
+    end
+
+    return params
+end
+
+local function safeCallback(callback)
+    if nil == callback then
+        return function() end
+    end
+
+    assert(type(callback) == "function", "A callback is expected")
+
+    return callback
+end
+
+---
+-- Execute a query with no result required, sync version
+--
+-- @param query
+-- @param params
+--
+-- @return int Number of rows updated
+--
+function MySQL.Sync.execute(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_sync_execute(query, safeParameters(params))
+end
+
+---
+-- Execute a query and fetch all results in an sync way
+--
+-- @param query
+-- @param params
+--
+-- @return table Query results
+--
+function MySQL.Sync.fetchAll(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_sync_fetch_all(query, safeParameters(params))
+end
+
+---
+-- Execute a query and fetch the first column of the first row, sync version
+-- Useful for count function by example
+--
+-- @param query
+-- @param params
+--
+-- @return mixed Value of the first column in the first row
+--
+function MySQL.Sync.fetchScalar(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_sync_fetch_scalar(query, safeParameters(params))
+end
+
+---
+-- Execute a query and retrieve the last id insert, sync version
+--
+-- @param query
+-- @param params
+--
+-- @return mixed Value of the last insert id
+--
+function MySQL.Sync.insert(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_sync_insert(query, safeParameters(params))
+end
+
+---
+-- Execute a List of querys and returns bool true when all are executed successfully
+--
+-- @param querys
+-- @param params
+--
+-- @return bool if the transaction was successful
+--
+function MySQL.Sync.transaction(querys, params)
+    assert(type(querys) == "table", "The SQL Query must be a table of strings")
+
+    return exports['mysql-async']:mysql_sync_transaction(querys, safeParameters(params))
+end
+
+---
+-- Execute a query with no result required, async version
+--
+-- @param query
+-- @param params
+-- @param func(int)
+--
+function MySQL.Async.execute(query, params, func)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    exports['mysql-async']:mysql_execute(query, safeParameters(params), safeCallback(func))
+end
+
+---
+-- Execute a query and fetch all results in an async way
+--
+-- @param query
+-- @param params
+-- @param func(table)
+--
+function MySQL.Async.fetchAll(query, params, func)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    exports['mysql-async']:mysql_fetch_all(query, safeParameters(params), safeCallback(func))
+end
+
+---
+-- Execute a query and fetch the first column of the first row, async version
+-- Useful for count function by example
+--
+-- @param query
+-- @param params
+-- @param func(mixed)
+--
+function MySQL.Async.fetchScalar(query, params, func)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    exports['mysql-async']:mysql_fetch_scalar(query, safeParameters(params), safeCallback(func))
+end
+
+---
+-- Execute a query and retrieve the last id insert, async version
+--
+-- @param query
+-- @param params
+-- @param func(string)
+--
+function MySQL.Async.insert(query, params, func)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    exports['mysql-async']:mysql_insert(query, safeParameters(params), safeCallback(func))
+end
+
+---
+-- Execute a List of querys and returns bool true when all are executed successfully
+--
+-- @param querys
+-- @param params
+-- @param func(bool)
+--
+function MySQL.Async.transaction(querys, params, func)
+    assert(type(querys) == "table", "The SQL Query must be a table of strings")
+
+    return exports['mysql-async']:mysql_transaction(querys, safeParameters(params), safeCallback(func))
+end
+
+---
+-- Execute a query with no result required, Threaded version
+--
+-- @param query
+-- @param params
+--
+-- @return int Number of rows updated
+--
+function MySQL.Threaded.execute(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_threaded_execute(query, safeParameters(params))
+end
+
+---
+-- Execute a query and fetch all results in an Threaded way
+--
+-- @param query
+-- @param params
+--
+-- @return table Query results
+--
+function MySQL.Threaded.fetchAll(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_threaded_fetch_all(query, safeParameters(params))
+end
+
+---
+-- Execute a query and fetch the first column of the first row, Threaded version
+-- Useful for count function by example
+--
+-- @param query
+-- @param params
+--
+-- @return mixed Value of the first column in the first row
+--
+function MySQL.Threaded.fetchScalar(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_threaded_fetch_scalar(query, safeParameters(params))
+end
+
+---
+-- Execute a query and retrieve the last id insert, Threaded version
+--
+-- @param query
+-- @param params
+--
+-- @return mixed Value of the last insert id
+--
+function MySQL.Threaded.insert(query, params)
+    assert(type(query) == "string", "The SQL Query must be a string")
+
+    return exports['mysql-async']:mysql_threaded_insert(query, safeParameters(params))
+end
+
+
+local isReady = false
+
+AddEventHandler('onMySQLReady', function ()
+    isReady = true
+end)
+
+function MySQL.ready(callback)
+    if isReady then
+        callback()
+
+        return
+    end
+
+    AddEventHandler('onMySQLReady', callback)
+end
+
+-- @copyright plesalex100 --
+
+function MySQL.isReady()
+    return isReady
+end
+
+local MySQLCommands = {}
+function MySQL.createCommand(name, command)
+    if not MySQLCommands[name] then
+        MySQLCommands[name] = command
+    end
+end
+
+function MySQL.execute(name, args, cb)
+    MySQL.ready(function()
+        if MySQLCommands[name] then
+            local args2 = {}
+            if args then
+                for k, v in pairs(args) do
+                    args2['@'..k] = v
+                end
+            end
+            MySQL.Sync.execute(MySQLCommands[name], args2)
+            if cb then
+                local task = Task(cb)
+                task({})
+            end
+        else
+            print("^1MySQL: error the command "..name.." doesn't exist")
+        end
+    end)
+end
+
+function MySQL.query(name, args, cb)
+    if isReady then
+        if MySQLCommands[name] then
+            local args2 = {}
+            if args then
+                for k, v in pairs(args) do
+                    args2['@'..k] = v
+                end
+            end
+            if cb then
+                local task = Task(cb)
+                local rws = MySQL.Sync.fetchAll(MySQLCommands[name], args2)
+                task({rws})
+            else
+                MySQL.Sync.execute(MySQLCommands[name], args2)
+            end
+        else
+            print("^1MySQL: error the command "..name.." doesn't exist")
+        end
+    else
+        print("^1MySQL: error the query "..name.." in resource "..GetCurrentResourceName().." must be placed in MySQL.ready() function")
+    end
+end
+
+return MySQL
